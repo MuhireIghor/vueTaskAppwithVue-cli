c284f4da87bb519bee0422f6e3d94d3363e932e3
MuhireIghor <muhireighor123@gmail.com>
2022-11-13 19:52:05 +0200

    Button and header component

---
+++ src/components/Button.vue
@@ -0,0 +1,17 @@
+<template>
+<button class='btn':style="{background:color}" @click='onClick()'>{{text }}</button>
+</template>
+<sript>
+export default {
+    name:'Button',
+    props:{
+        text:String,
+        color:String
+    },
+    methods:{
+        onClick(){
+            console.log("clicked")
+        }
+    }
+}
+</sript>
---
+++ src/components/Header.vue
@@ -0,0 +1,29 @@
+<template>
+<div class='header'>
+<h1>{{title}}</h1>
+</div>
+<Button text='Add Task' color='green'  />
+</template>
+<script>
+import Button from './Button'
+export default{
+    name:'Header',
+    props:{
+        title:{
+            type:String,
+            default:'Hello world'
+        }
+    },
+    components:{
+        Button
+    }
+}
+</script>
+<style scoped>
+.header{
+    align-items:'center';
+    justify-content:'space-between';
+    display:'flex';
+    margin-bottom:'20px';
+}
+</style>
