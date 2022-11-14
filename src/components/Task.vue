46fe370e43d7eb8fafbb52a1c8297968dd2db2f9
MuhireIghor <muhireighor123@gmail.com>
2022-11-14 14:23:18 +0200

    Task component data sharing

---
+++ src/components/Task.vue
@@ -0,0 +1,15 @@
+<template>
+<div >
+    <h3>{{task.text}}</h3>
+    <h3>{{task.day}}</h3>
+</div>
+</template>
+<script>
+export default{
+    name:"Task",
+    props:{
+        task:Object
+    }
+}
+
+</script>
