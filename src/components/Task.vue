c4bae51b53f5b3509f4b90f4aab366b8bb9062b6
MuhireIghor <muhireighor123@gmail.com>
2022-11-15 06:39:19 +0200

    toggleReminder

--- src/components/Task.vue
+++ src/components/Task.vue
@@ -1,6 +1,8 @@
 <template>
-<div >
-    <h3>{{task.text}}</h3>
+<div @dblclick = "$emit('toggle-Reminder',task.id)" :class="[task.reminder?'reminder':'']">
+    <h3>{{task.text}}
+    <p class="fas fas-times" @click="deleteTask(task.id)" > x</p>
+    </h3>
     <h3>{{task.day}}</h3>
 </div>
 </template>
@@ -9,7 +11,21 @@ export default{
     name:"Task",
     props:{
         task:Object
-    }
+    },
+    methods:{
+        deleteTask(id){
+            this.$emit('delete-task',id);//for passing id to the upper component  with respcetive data about a task with a certain Id
+            }
+    },
+    emits:[
+        'toggle-Reminder','delete-task'
+    ]
+ 
 }
 
 </script>
+<style>
+.reminder{
+    border-left:4px solid green;
+}
+</style>
